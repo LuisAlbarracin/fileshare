be3f79542e95a5fb78b85300597ce04bf61526d5
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2022-04-18 16:21:12 -0500

    version AyD

---
+++ about.php
@@ -0,0 +1,59 @@
+<?php 
+    
+
+    $active7="active"; 
+    include "header.php"; 
+?>
+    
+    <div class="content-wrapper"><!-- Content Wrapper. Contains page content -->
+        <section class="content-header"><!-- Content Header (Page header) -->
+            <h1>Sobre el autor</h1>
+            <ol class="breadcrumb">
+                <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Sobre el autor</li>
+            </ol>
+        </section>
+        <section class="content"><!-- Main content -->
+            <div class="row"><!-- Small boxes (Stat box) -->
+                <div class="col-md-6 col-md-offset-3">
+                    <h1>Acerca del autor</h1>
+                    <p> <strong></strong> ( creador de <a href="#" target="_blank">FileShare Sistema Para Compartir Archivos</a>.</p>
+
+                    <br>
+                        <p style="color: red"><a href="#" target="_blank"></a></p>
+                    <br>
+
+                    <h2>- <small>Compartir archivos</small></h2>
+                    <p>FileShare es un sistema para compartir archivos o informes de gestion de semilleros FINU.</p>
+
+                    <h1 style="text-align: center;"></h1>
+                    <h3>Descripcion</h3>
+                    <p> Sistema Para Compartir Archivos es un sistema con muchas mejoras, para compartir archivos en el que puedes rellenar tu perfil, subir cualquier tipo de archivo y sobre todo crear carpetas, buscar con quien compartirle (admin), concedir permisos a los archivos/carpetas compañeros, comentar las publicaciones, recibir notificaciones y mucho mas.</p>
+
+
+                    <h3>Modulos</h3>
+                    <p>Defino a grandes rasgos los modulos generales del sistema</p>
+                    <ul>
+                        <li><b>Usuarios</b>: Se pueden registrar para acceder a sus cuentas y asi empezar la aventura con FileShare.</li>
+                        <li><b>Subir archivos</b>: Cada usuario puede subir lo que quiera y lo visualizara en su muro, el cual podran ver tambien sus amigos si les concede los permisos.</li>
+                        <li><b>Crear carpetas</b>: Cada usuario puede crear carpetas y lo visualizara en su muro, el cual podran ver tambien sus amigos si les concede los permisos.</li>
+                        <li><b>Perfiles</b>: Los usuarios pueden rellenar su perfil</li>
+                        <li><b>Comentarios</b>: Los usuarios pueden escribir comentarios a los archivos y/o imagenes de sus amigos.</li>
+                        <li><b>Notificaciones</b>: recibe notificaciones cuando tus amigos descarguen o comentan tus archivos y/o carpetas.</li>
+                        <li><b>Descargas publicas</b>: Puedes compartir el enlace con amigos que no esten registrados.</li>
+                        <li><b>Descargas Privadas</b>: Puedes compartir el enlace con amigos que esten registrados en el sistema.</li>
+                        <li><b>Papelera de reciclaje</b>: Tus archivos no se eliminaran definitivamente, se iran a la papelera de reciclaje.</li>
+                       
+                        <li><b>Y muchas cosas más.</b></li>
+                    </ul>
+
+                    <br><br>
+                    <p><b>email:</b> </p>
+
+                    <h1 style="text-align: center;"><a href="#" target="_blank">#</a></h1>
+                </div>
+            </div><!-- /.row -->
+        </section>
+    </div><!-- /.content -->
+
+<?php include "footer.php"; ?>
---
+++ cpanel/action/logout.php
@@ -0,0 +1,16 @@
+<?php
+    
+   
+
+session_start();
+
+// -- eliminamos el usuario
+if(isset($_SESSION['admin_id'])){
+	unset($_SESSION['admin_id']);
+}
+
+//session_destroy();
+
+header("location: ../");
+
+?>
